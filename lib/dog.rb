4f94e28e0da981723eab7487e771cdd4a25e60cc
sir-udenna <udennaboy@gmail.com>
2021-02-27 02:54:35 -0600

    commit tree 6e0c85e8f8326bfe6466ed51d0a22eff76305dd7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,34 @@
 class Dog
+  @@all = []
   # code goes here
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def owner
+    @owner
+  end
+
+  def mood
+    @mood
+  end
+
+  def mood=(new_mood)
+    @mood = new_mood
+  end
+
+  def owner=(new_owner)
+    @owner = new_owner
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
+require 'pry'
 class Owner
-  # code goes here
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def name 
+    @name
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select do | cat |
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do | dog |
+      dog.owner == self
+    end
+  end
+
+  def buy_cat
+  end
 end
